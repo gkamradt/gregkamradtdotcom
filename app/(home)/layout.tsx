75fbc684575c83842f7d125eec802bc46dbcad18
Greg Kamradt <gkamradt@gmail.com>
2024-09-16 17:03:58 -0700

    updates

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,16 @@
 import type { ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/home-layout';
 import { baseOptions } from '../layout.config';
+import { baseUrl, createMetadata } from '@/utils/metadata';
+
+export const metadata = createMetadata({
+  title: {
+    template: '%s | Greg Kamradt',
+    default: 'Greg Kamradt',
+  },
+  description: 'Personal website for Greg Kamradt',
+  metadataBase: baseUrl,
+});
 
 export default function Layout({
   children,
